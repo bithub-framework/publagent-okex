256e0c5331b25038ebb4c5ff16000487f81306ff
Zim <zimtsui@gmail.com>
2019-10-19 15:37:36 +0800

    dev

--- procfile.js
+++ procfile.js
@@ -13,16 +13,15 @@ module.exports = (pandora) => {
         });
 
     pandora
-        .service(
-            'kita',
-            PandoraKita
-        ).process('weak-all');
+        .service('kita', PandoraKita)
+        .process('weak-all');
 
     pandora
         .service(
             'quote-agent-okex-websocket',
             autoExitDecorator(3000)(QuoteAgentOkexWebsocket),
-        ).process('process1');
+        ).dependency('kita')
+        .process('process1');
 
     /**
      * you can also use cluster mode to start application
