9d4a2bc27c62b9a09983fc6333efc35cae37727c
Zim <zimtsui@gmail.com>
2020-12-22 16:11:55 +0800

    dev

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,10 @@
 import { adaptor } from 'startable';
 import PublicAgent from './public-agent';
+console.log = (...args) => {
+    console.info(new Date());
+    console.info(...args);
+};
+console.log('Starting');
 const publicAgent = new PublicAgent();
 adaptor(publicAgent);
 publicAgent.start().then(() => {
--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,13 @@
 import { adaptor } from 'startable';
 import PublicAgent from './public-agent';
 
+console.log = (...args: any[]) => {
+    console.info(new Date());
+    console.info(...args);
+}
+
+console.log('Starting');
+
 const publicAgent = new PublicAgent();
 adaptor(publicAgent);
 
