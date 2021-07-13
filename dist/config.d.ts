cb368be552d96eac4ad6c56fe50dcb9953859b67
Zim <zimtsui@gmail.com>
2021-07-14 07:44:30 +0800

    commonjs

--- dist/config.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const OKEX_WEBSOCKET_URL = "wss://real.okex.com:8443/ws/v3";
-export declare const EXCHANGE_NAME = "okex";
-export declare const PING_TIMEOUT = 5000;
-export declare const PONG_TIMEOUT = 5000;
